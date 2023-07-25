88d9b888d55d72f857200f0a6557700be5cbeeeb
Marwan <mystic21003@gmail.com>
2023-07-25 21:11:29 +0300

    Update index.py

--- index.py
+++ index.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 
 import requests
 
-
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
